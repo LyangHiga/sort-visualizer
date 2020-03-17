83a165b48ad9470cc8eb1eb7708223bd5a48b5f6
LyangHiga <lyang_higa@poli.ufrj.br>
2020-03-17 15:37:41 -0300

    Displaying bars and buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import SortVisualizer from "./sortVisualizer";
+import SortVisualizer from "./SortVisualizer";
 
 function App() {
   return (
--- src/sortVisualizer.js
+++ src/sortVisualizer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { randomIntFromInterval } from "./arrHelper";
+import "./SortVisualizer.css";
 
 export default function SortVisualizer(props) {
   const [arr, setArr] = useState([]);
@@ -11,22 +12,26 @@ export default function SortVisualizer(props) {
 
   const resetArr = () => {
     const newArr = [];
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 400; i++) {
       // 5 and 730 to fit in the screen
       // duplicated values are allowed
-      newArr.push(randomIntFromInterval(5, 730));
+      newArr.push(randomIntFromInterval(5, 780));
     }
     setArr(newArr);
   };
 
   return (
     <div>
-      <h1>Sort Visualizer</h1>
-      {arr.map((val, i) => (
-        <div className="arr-bar" key={i}>
-          {val}
-        </div>
-      ))}
+      <h1>Sorting Visualizer</h1>
+      <div className="btns">
+        <button onClick={() => resetArr()}>New Bars</button>
+        <button>Merge Sort</button>
+      </div>
+      <div className="arr-container">
+        {arr.map((val, i) => (
+          <div className="arr-bar" key={i} style={{ height: `${val}px` }}></div>
+        ))}
+      </div>
     </div>
   );
 }
