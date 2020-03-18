6c9c1197f5140c88d9db7a884f27996526817c6a
LyangHiga <lyang_higa@poli.ufrj.br>
2020-03-18 16:22:43 -0300

    Add Bubble Sort and testing

--- src/arrHelper.js
+++ src/arrHelper.js
@@ -9,3 +9,18 @@ export const arrAreEquals = (arr1, arr2) => {
   }
   return true;
 };
+
+export const testSortingAlgorithms = sortMethod => {
+  // get 100 random arrays
+  for (let i = 0; i < 100; i++) {
+    const array = [];
+    //   random length between 1 and 1000
+    const length = randomIntFromInterval(1, 1000);
+    for (let i = 0; i < length; i++) {
+      array.push(randomIntFromInterval(-1000, 1000));
+    }
+    const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
+    const sortedArray = sortMethod(array.slice());
+    console.log(arrAreEquals(javaScriptSortedArray, sortedArray));
+  }
+};
--- src/sortVisualizer.js
+++ src/sortVisualizer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { randomIntFromInterval } from "./arrHelper";
+import { randomIntFromInterval, testSortingAlgorithms } from "./arrHelper";
 import "./SortVisualizer.css";
 
 export default function SortVisualizer(props) {
@@ -20,12 +20,31 @@ export default function SortVisualizer(props) {
     setArr(newArr);
   };
 
+  // check consective elements (arr[j],arr[j+1]) , if the arr[j] > arr[j+1] swap!
+  // in each iterations we sort from the end
+  const bubbleSort = arr => {
+    let noSwaps;
+    for (let i = arr.length; i > 0; i--) {
+      noSwaps = true;
+      for (let j = 0; j < i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+          noSwaps = false;
+        }
+      }
+      if (noSwaps) break;
+    }
+    return arr;
+  };
+
   return (
     <div>
       <h1>Sorting Visualizer</h1>
       <div className="btns">
         <button onClick={() => resetArr()}>New Bars</button>
-        <button>Merge Sort</button>
+        <button onClick={() => bubbleSort(arr)}>BubbleSort Sort</button>
+        {/* choose wich method will be tested */}
+        <button onClick={() => testSortingAlgorithms(bubbleSort)}>test</button>
       </div>
       <div className="arr-container">
         {arr.map((val, i) => (
