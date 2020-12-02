93d29b99e155933073105b089594741caa4c753b
lyanghiga <lyang_higa@poli.ufrj.br>
2020-12-02 11:48:54 -0300

    Refactoring get window size with use window size hook

--- src/sortVisualizer.js
+++ src/sortVisualizer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import useWindowDimensions from "./hooks/useWindowDim";
 import { randomIntFromInterval } from "./arrHelper";
 import "./SortVisualizer.css";
 import { changeAllColor } from "./animations";
@@ -16,6 +17,7 @@ const NAV_HEIGHT = 200;
 export default function SortVisualizer(props) {
   const [arr, setArr] = useState([]);
   const [disable, setDisable] = useState(false);
+  const { height, width } = useWindowDimensions();
 
   useEffect(() => {
     resetArr();
@@ -24,9 +26,6 @@ export default function SortVisualizer(props) {
 
   const resetArr = () => {
     const newArr = [];
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    console.log(height);
     const nbars = Math.floor((width - 2 * MARGIN) / BAR_SIZE);
     for (let i = 0; i < nbars; i++) {
       // 5 and 730 to fit in the screen
